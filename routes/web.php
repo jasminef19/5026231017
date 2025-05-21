63de632857fbb58aa19fc931bda1eba5d57e7b7a
jasminef19 <5026231017@student.its.ac.id>
2025-05-21 12:35:31 +0700

    pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Link ;
+use App\Http\Controllers\PegawaiController ;
+use App\Http\Controllers\BlogController ;
 
 /*
 |--------------------------------------------------------------------------
@@ -64,3 +66,15 @@ Route::get('template1', function () {
 Route::get('index', function () {
 	return view('index');
 });
+
+Route::get('dosen', [Link::class, 'index'] );
+
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index'] );
+
+Route::get('/formulir', [PegawaiController::class, 'formulir'] );
+Route::post('/formulir/proses', [PegawaiController::class, 'proses'] );
+
+// route blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
